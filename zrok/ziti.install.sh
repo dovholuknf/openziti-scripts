739a97b282f1792f0883f5990da66223f12636a3
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-01-02 15:35:18 -0500

    updats

--- zrok/ziti.install.sh
+++ zrok/ziti.install.sh
@@ -23,15 +23,14 @@ sudo systemctl enable --now ziti-router
 sudo systemctl -q status ziti-controller --lines=0 --no-pager
 sudo systemctl -q status ziti-router --lines=0 --no-pager
 
-echo "cloning ZAC"
-git clone https://github.com/openziti/ziti-console.git "${ZITI_HOME}/ziti-console"
-cd "${ZITI_HOME}/ziti-console"
-npm install
-#ln -s "${ZITI_PKI}/${ZITI_EDGE_CONTROLLER_HOSTNAME}-intermediate/certs/${ZITI_EDGE_CONTROLLER_HOSTNAME}-server.chain.pem" "${ZITI_HOME}/ziti-console/server.chain.pem"
-#ln -s "${ZITI_PKI}/${ZITI_EDGE_CONTROLLER_HOSTNAME}-intermediate/keys/${ZITI_EDGE_CONTROLLER_HOSTNAME}-server.key" "${ZITI_HOME}/ziti-console/server.key"
-
-ln -s "${LE_CHAIN}" "${ZITI_HOME}/ziti-console/server.chain.pem"
-ln -s "${LE_KEY}" "${ZITI_HOME}/ziti-console/server.key"
+#diabled jan 2 echo "cloning ZAC"
+#diabled jan 2 git clone https://github.com/openziti/ziti-console.git "${ZITI_HOME}/ziti-console"
+#diabled jan 2 cd "${ZITI_HOME}/ziti-console"
+#diabled jan 2 npm install
+#diabled jan 2 #ln -s "${ZITI_PKI}/${ZITI_EDGE_CONTROLLER_HOSTNAME}-intermediate/certs/${ZITI_EDGE_CONTROLLER_HOSTNAME}-server.chain.pem" "${ZITI_HOME}/ziti-console/server.chain.pem"
+#diabled jan 2 #ln -s "${ZITI_PKI}/${ZITI_EDGE_CONTROLLER_HOSTNAME}-intermediate/keys/${ZITI_EDGE_CONTROLLER_HOSTNAME}-server.key" "${ZITI_HOME}/ziti-console/server.key"
+#diabled jan 2 ln -s "${LE_CHAIN}" "${ZITI_HOME}/ziti-console/server.chain.pem"
+#diabled jan 2 ln -s "${LE_KEY}" "${ZITI_HOME}/ziti-console/server.key"
 
 #sed -i 's#^      server_cert\: .*#      server_cert\: "'${LE_CHAIN}'"#g' $ZITI_HOME/$ZITI_NETWORK.yaml
 #sed -i 's#^      key\:         .*#      key\:         "'${LE_KEY}'"#g' $ZITI_HOME/$ZITI_NETWORK.yaml
@@ -46,10 +45,10 @@ sudo systemctl restart ziti-controller
 sudo systemctl restart ziti-router
 
 
-createZacSystemdFile
-sudo cp "${ZITI_HOME}/ziti-console.service" /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable --now ziti-console
+#diabled jan 2 createZacSystemdFile
+#diabled jan 2 sudo cp "${ZITI_HOME}/ziti-console.service" /etc/systemd/system
+#diabled jan 2 sudo systemctl daemon-reload
+#diabled jan 2 sudo systemctl enable --now ziti-console
 #sudo systemctl restart ziti-controller
 #sudo systemctl restart ziti-router
 
