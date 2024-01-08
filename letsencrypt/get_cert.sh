f6a09ffe578f628c7a3d26f3be8060676e4bb7cd
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-01-08 15:10:42 -0500

    updates

--- letsencrypt/get_cert.sh
+++ letsencrypt/get_cert.sh
@@ -1,9 +1,21 @@
-your_email="clint@openziti.org"
-wildcard_url="clint.demo.openziti.org"
+exitWithError() {
+    echo -e "Error: $1"
+    exit 1
+}
+
+if [ "$#" -lt 2 ]; then
+    exitWithError "Two parameters are required. \
+    \n       The first parameter should be the email you want to be notified when the cert is expiring \
+    \n       The second is the domain you want a wildcard cert for\n"
+fi
+
+your_email=$1
+wildcard_url=$2
+
 sudo docker run -it --rm --name certbot \
   -v "/data/docker/letsencrypt:/etc/letsencrypt" \
-certbot/certbot certonly -d "*.${wildcard_url}" \
---manual \
---preferred-challenges dns \
---email "${your_email}" \
---agree-tos
+  certbot/certbot certonly -d "*.${wildcard_url}" \
+  --manual \
+  --preferred-challenges dns \
+  --email "${your_email}" \
+  --agree-tos
