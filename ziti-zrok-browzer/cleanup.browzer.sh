df7b4b94e4192397c447ad5822055e5c180284c9
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-01-08 14:22:11 -0500

    update

--- ziti-zrok-browzer/cleanup.browzer.sh
+++ ziti-zrok-browzer/cleanup.browzer.sh
@@ -2,9 +2,6 @@ echo "souring env file at $HOME/.ziti/quickstart/$(hostname)/$(hostname).env"
 source $HOME/.ziti/quickstart/$(hostname)/$(hostname).env
 ziti edge login -u $ZITI_USER -p $ZITI_PWD -y $ZITI_EDGE_CTRL_ADVERTISED_HOST_PORT
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source $SCRIPT_DIR/ziti-zrok-browzer.env
-
 ziti edge delete identity clint.dovholuk@netfoundry.io
 ziti edge delete identity curt.tudor@netfoundry.io
 
