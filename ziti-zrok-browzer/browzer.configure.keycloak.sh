378bf55048dfcf7a6debf68ce67e06c15e3f459b
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-01-08 21:14:32 -0500

    updates

--- ziti-zrok-browzer/browzer.configure.keycloak.sh
+++ ziti-zrok-browzer/browzer.configure.keycloak.sh
@@ -1,4 +1,10 @@
-function kcadm { docker compose -f ${SCRIPT_DIR}/${ZITI_BROWZER_DOCKER_PROJECT}.yml --project-name ${ZITI_BROWZER_DOCKER_PROJECT} exec -it browzer-keycloak /opt/keycloak/bin/kcadm.sh $@; }
+function kcadm {
+  docker compose 
+    -f ${SCRIPT_DIR}/${ZITI_BROWZER_DOCKER_PROJECT}.yml 
+    --project-name ${ZITI_BROWZER_DOCKER_PROJECT} 
+    exec -it browzer-keycloak 
+    /opt/keycloak/bin/kcadm.sh $@;
+}
 
 kcadm config credentials \
   --server ${KEYCLOAK_HOST_AND_PORT} \
@@ -10,23 +16,12 @@ kcadm create realms \
   -s realm=${KEYCLOAK_REALM} \
   -s enabled=true
 
-kcadm create identity-provider/instances \
-  -r ${KEYCLOAK_REALM} \
-  -s alias=github-oidc \
-  -s providerId=github \
-  -s enabled=true \
-  -s 'config.useJwksUrl="true"' \
-  -s 'config.authorizationUrl=https://github.com/login/oauth/authorize' \
-  -s 'config.tokenUrl=https://github.com/login/oauth/access_token' \
-  -s 'config.userInfoUrl=https://api.github.com/user' \
-  -s config.clientId=${ZITI_BROWZER_GITHUB_CLIENT} \
-  -s config.clientSecret=${ZITI_BROWZER_GITHUB_CLIENTSECRET}
-
 kcadm create clients \
   -r ${KEYCLOAK_REALM} \
   -s clientId=${ZITI_BROWZER_CLIENT_ID} \
   -s protocol=openid-connect \
-  -s 'redirectUris=["https://'${ZITI_BROWZER_VHOST}'/*"]' \
+  -s 'redirectUris=["https://'${ZITI_BROWZER_VHOST}'/*","https://'${KEYCLOAK_BASE}:${TEST_PORT}'/*","http://localhost:8080/*"]' \
+  -s 'webOrigins=["https://'${ZITI_BROWZER_VHOST}'","https://'${KEYCLOAK_BASE}:${TEST_PORT}'","http://localhost:8080"]' \
   -s 'directAccessGrantsEnabled=true'
 
 CLIENT_SCOPE_ID=$(kcadm get clients -r ${KEYCLOAK_REALM} | jq -r '.[] | select(.clientId == "'${ZITI_BROWZER_CLIENT_ID}'") | .id')
@@ -41,4 +36,24 @@ kcadm create clients/${CLIENT_SCOPE_ID}/protocol-mappers/models \
   -s config.\"access.token.claim\"=\"true\" \
   -s config.\"id.token.claim\"=\"false\"
 
+if [[ "${ZITI_BROWZER_GITHUB_CLIENTSECRET}" != "" ]]; then
+  kcadm create identity-provider/instances \
+    -r ${KEYCLOAK_REALM} \
+    -s alias=github-oidc \
+    -s providerId=github \
+    -s enabled=true \
+    -s 'config.useJwksUrl="true"' \
+    -s config.clientId=${ZITI_BROWZER_GITHUB_CLIENT} \
+    -s config.clientSecret=${ZITI_BROWZER_GITHUB_CLIENTSECRET}
+fi
 
+if [[ "${ZITI_BROWZER_GOOGLE_CLIENTSECRET}" != "" ]]; then
+  kcadm create identity-provider/instances \
+    -r ${KEYCLOAK_REALM} \
+    -s alias=google-oidc \
+    -s providerId=google \
+    -s enabled=true \
+    -s 'config.useJwksUrl="true"' \
+    -s config.clientId=${ZITI_BROWZER_GOOGLE_CLIENT} \
+    -s config.clientSecret=${ZITI_BROWZER_GOOGLE_CLIENTSECRET}
+fi
