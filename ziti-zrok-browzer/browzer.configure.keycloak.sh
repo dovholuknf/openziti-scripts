23b0b833ce50db8130b53577d58de8bf19b5f6ec
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-01-11 11:38:20 -0500

    update

--- ziti-zrok-browzer/browzer.configure.keycloak.sh
+++ ziti-zrok-browzer/browzer.configure.keycloak.sh
@@ -21,7 +21,7 @@ kcadm create clients \
   -s clientId=${ZITI_BROWZER_CLIENT_ID} \
   -s protocol=openid-connect \
   -s 'redirectUris=["https://'${ZITI_BROWZER_VHOST}'/*","https://brozac.'${WILDCARD_DNS}'/*","https://'${KEYCLOAK_BASE}:${TEST_PORT}'/*","http://localhost:8080/*"]' \
-  -s 'webOrigins=["https://'${ZITI_BROWZER_VHOST}'","https://brozac.'${WILDCARD_DNS}',"https://'${KEYCLOAK_BASE}:${TEST_PORT}'","http://localhost:8080"]' \
+  -s 'webOrigins=["https://'${ZITI_BROWZER_VHOST}'","https://brozac.'${WILDCARD_DNS}'","https://'${KEYCLOAK_BASE}:${TEST_PORT}'","http://localhost:8080"]' \
   -s 'directAccessGrantsEnabled=true'
 
 CLIENT_SCOPE_ID=$(kcadm get clients -r ${KEYCLOAK_REALM} | jq -r '.[] | select(.clientId == "'${ZITI_BROWZER_CLIENT_ID}'") | .id')
