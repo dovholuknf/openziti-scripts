b0c1878b417c5e44de85e42e00ee68807fb878f0
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-04-09 11:48:28 -0400

    update download zac

--- ziti-zrok-browzer/downloaded.zac.sh
+++ ziti-zrok-browzer/downloaded.zac.sh
@@ -1,7 +1,7 @@
 ZAC_VERSION=3.0.8
 mkdir -p $ZITI_HOME/zac/downloads
 wget -O $ZITI_HOME/zac/downloads/zac-${ZAC_VERSION}.zip https://github.com/openziti/ziti-console/releases/download/app-ziti-console-v${ZAC_VERSION}/ziti-console.zip
-unzip $ZITI_HOME/zac/zac.zip -d $ZITI_HOME/zac/${ZAC_VERSION}
+unzip $ZITI_HOME/zac/downloads/zac-${ZAC_VERSION}.zip -d $ZITI_HOME/zac/${ZAC_VERSION}
 cat <<HERE
 
 Now update your controller config. Your file is located at:
