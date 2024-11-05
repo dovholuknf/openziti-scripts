bd75cd9521d447b9f9b8c5033085b06b152590e8
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-11-05 08:37:32 -0500

    bind port

--- haproxy/install-ziti-using-quickstart.sh
+++ haproxy/install-ziti-using-quickstart.sh
@@ -11,6 +11,7 @@ export ZITI_ROUTER_ADVERTISED_ADDRESS="haper.${base_dns}"
 export ZITI_CTRL_ADVERTISED_PORT=10443
 export ZITI_CTRL_EDGE_ADVERTISED_PORT=10443
 export ZITI_ROUTER_PORT=11443
+export ZITI_ROUTER_LISTENER_BIND_PORT=11443
 read -sp "Enter ZITI_PWD: " ZITI_PWD && export ZITI_PWD
 echo
 source /dev/stdin <<< "$(wget -qO- https://get.openziti.io/ziti-cli-functions.sh)"; expressInstall
