75a885b96090dabcdeb764a87ea57898588b45b7
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-11-13 17:10:47 -0500

    add timeouts

--- zrok/ziti.cleanup.sh
+++ zrok/ziti.cleanup.sh
@@ -1,10 +1,10 @@
-sudo systemctl stop ziti-console
-sudo systemctl stop ziti-router
-sudo systemctl stop ziti-controller
+sudo systemctl --timeout=10 stop ziti-console
+sudo systemctl --timeout=10 stop ziti-router
+sudo systemctl --timeout=10 stop ziti-controller
 sudo rm -rf $HOME/.ziti/quickstart
-sudo systemctl disable ziti-console
-sudo systemctl disable ziti-router
-sudo systemctl disable ziti-controller
+sudo systemctl --timeout=10 disable ziti-console
+sudo systemctl --timeout=10 disable ziti-router
+sudo systemctl --timeout=10 disable ziti-controller
 source /dev/stdin <<< "$(wget -qO- https://get.openziti.io/quick/ziti-cli-functions.sh)";
 
 unsetZitiEnv
